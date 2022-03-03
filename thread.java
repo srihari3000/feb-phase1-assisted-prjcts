13ed76fc0e2e7733486feb1f58a25eb0a4e741b2
srihari <jujjavarpusrihari1411@gmail.com>
2022-03-03 09:36:20 +0530

    first commit

---
+++ thread.java
@@ -0,0 +1,13 @@
+public class thread extends Thread
+{
+ 	public void run()
+ 	{
+  		System.out.println("concurrent thread started running..");
+}
+ 	public static void main( String args[] )
+ 	{
+  		thread mt = new  thread();
+  		mt.start();
+ 	}
+}
+
